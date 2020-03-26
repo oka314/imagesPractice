ea5f813d6fbdf120e65e03853f55a979ac6f31ae
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-26 17:51:46 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
+  resources :products, except: :show
 end
