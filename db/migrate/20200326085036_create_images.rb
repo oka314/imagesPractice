49c45885271203954b7035dbe14d29c1fc8caa0f
oka314 <61148080+oka314@users.noreply.github.com>
2020-03-26 17:58:09 +0900

    Update 20200326085036_create_images.rb

--- db/migrate/20200326085036_create_images.rb
+++ db/migrate/20200326085036_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-
+      t.string :src
+      t.references :product, forein_key: true
       t.timestamps
     end
   end
